b1ffe32db6bab184cff883b52c7fb6c83b2f6166
Tokarev <egor.tokarev@sap.com>
2018-10-20 14:50:01 +0300

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get('/todos/:id',(req,res)=>{
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        res.status(404).send();
+        return res.status(404).send();
     }    
 
     Todo.findById(id).then((todo)=>{
@@ -51,6 +51,24 @@ app.get('/todos/:id',(req,res)=>{
     }).catch((e)=>{
         res.status(400).send();
     });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }    
+
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if (!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+    
 });
 
 app.listen(port, ()=>{
