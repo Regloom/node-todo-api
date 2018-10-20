5e7e3d4883c553a6ae6a6a50d322d653d1b96949
Tokarev <egor.tokarev@sap.com>
2018-10-20 12:50:12 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -25,6 +25,13 @@ app.post('/todos', (req,res)=>{
 });
 
 //GET /todos/123
+app.get('/todos',(req,res) => {
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (e)=>{
+        res.status(400).send(e);
+    })
+});
 
 app.listen(3000, ()=>{
     console.log('Started on port 3000');
